371a6a3fba1ac35023554fcb0ee81acd3b40fb6b
Simran Singh <ssinhjxyz@users.noreply.github.com>
2015-11-07 21:42:15 -0500

    linear / db mag

--- wireless fading/js/globals.js
+++ wireless fading/js/globals.js
@@ -7,14 +7,10 @@ Constants.InitialNumVectors = 3;
 Object.freeze(Constants);
 
 // Globals
-var canvasHeight = 500;
-var canvasWidth = 500;
 var numVectors = Constants.InitialNumVectors;
 var vectors = [];
 var resultant;   
 var animationInterval = 16;
-var setIntervalId = -1;
-var svg;
-var sineSvg;
-var sinePoint;
+var magType = "db";
+
 
