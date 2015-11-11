42ea703a98d860b528d6275b084f51f5a8cd2ec3
ssinhjxyz <s.singh.xyz@gmail.com>
2015-11-10 20:19:16 -0500

    Added Setting for InterNodeDistance

--- network/js/settings.js
+++ network/js/settings.js
@@ -5,11 +5,12 @@ SETTINGS.FrameSlot = 0.050; //milli seconds
 SETTINGS.PacketSize = 1000; // In bytes
 SETTINGS.TransmissionRate = 100 // In Mbps
 SETTINGS.MeanInterPacket = 2;
-SETTINGS.SimTime = 1000;
+SETTINGS.SimTime = 5000;
 SETTINGS.InfinitesimalDelay = 0.001;
 SETTINGS.PropagationSpeed = 2*Math.pow(10,5);
 SETTINGS.ConvertToSec = Math.pow(10,-3);
 SETTINGS.ConvertToMilliSec = Math.pow(10,3);
+SETTINGS.InterNodeDistance = 2000;
 SETTINGS.TransmissionTime = ((SETTINGS.PacketSize*8)/(SETTINGS.TransmissionRate*Math.pow(10,6)))*SETTINGS.ConvertToMilliSec;
 
 
