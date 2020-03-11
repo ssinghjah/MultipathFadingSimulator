06e000d65bf4622902ae4f57532b98af28cf5ab9
Simran <ssingh28@ncsu.edu>
2020-03-11 01:27:31 -0400

    deleted unwanted folders

--- network/js/random.js
+++
@@ -1,7 +0,0 @@
-function CustomRandom(){
-} 
-
-// Returns a random integer between min (included) and max (included)
-CustomRandom.get = function(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
--- network/js/seedGenerator.js
+++
@@ -1,9 +0,0 @@
-function SeedGenerator(){
-	
-}
-
-SeedGenerator.Get = function(index){
-	var seedBase = new Random().uniform(1,1000);
-	var seed = seedBase*(index + 1);
-	console.log("Seed " + index + " : " + seed); 
-}
